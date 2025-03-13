da6750a520af7e9d6d2eed25c42e1b7bf0345c03
bludahlia <omihalic@purdue.edu>
2025-03-12 21:56:46 -0400

    creation

---
+++ components/navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav className = "navbar">
+            <ul>
+                <li><a href = "/"><img></img></a></li>
+                <li><a href = "#">Browse</a></li>
+                <li><a href = "#">Search bar</a></li>
+                <li><a href = "#">Login</a></li>
+                <li><button>bright</button></li>
+                <li><button>settings</button></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
